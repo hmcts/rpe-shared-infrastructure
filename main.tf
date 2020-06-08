cf710afb4ff237ec64f5831d4c72757fdf7017ad
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-06-08 14:18:04 +0100

    fix typo

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "key-vault" {
   object_id           = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
-  # dcd_cc-dev group object ID
+  # dcd_platformengineering group object ID
   product_group_object_id    = "c36eaede-a0ae-4967-8fed-0a02960b1370"
   common_tags                = "${var.common_tags}"
   managed_identity_object_id = "${var.managed_identity_object_id}"
