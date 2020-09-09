5c7662568a879d23148ec7d02076a553ae8952e5
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-09-09 14:52:07 +0100

    Update terraform

--- action_group.tf
+++ action_group.tf
@@ -1,16 +1,16 @@
 module "action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
-  env      = "${var.env}"
+  env      = var.env
 
-  resourcegroup_name     = "${azurerm_resource_group.rg.name}"
+  resourcegroup_name     = azurerm_resource_group.rg.name
   action_group_name      = "RPE alerts - ${var.env}"
   short_name             = "rpe_alerts"
   email_receiver_name    = "Platform engineering alerts"
-  email_receiver_address = "${data.azurerm_key_vault_secret.actionGroupEmail.value}"
+  email_receiver_address = data.azurerm_key_vault_secret.actionGroupEmail.value
 }
 
 data "azurerm_key_vault_secret" "actionGroupEmail" {
   name      = "actionGroupEmail"
-  key_vault_id = "${module.key-vault.key_vault_id}"
+  key_vault_id = module.key-vault.key_vault_id
 }
--- main.tf
+++ main.tf
@@ -1,24 +1,35 @@
-provider "azurerm" {
-  version = "1.44.0"
+terraform {
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.25"
+    }
+  }
+}
+
+provider azurerm {
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
-  location = "${var.location}"
+  location = var.location
 
-  tags = "${var.common_tags}"
+  tags = var.common_tags
 }
 
 module "key-vault" {
-  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
   product             = "${var.product}-shared"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  env                 = var.env
+  tenant_id           = var.tenant_id
+  object_id           = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_platformengineering group object ID
   product_group_object_id    = "c36eaede-a0ae-4967-8fed-0a02960b1370"
-  common_tags                = "${var.common_tags}"
+  common_tags                = var.common_tags
   create_managed_identity    = true
 }
