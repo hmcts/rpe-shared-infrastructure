fc08745caf90c07835e4cbff76f2a91056ed4a18
luigibk <41112628+luigibk@users.noreply.github.com>
2020-09-10 12:05:18 +0100

    Update terraform (#11)
    * Update terraform
    
    * Fix duplicate backend

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
 
 variable "product" {}
@@ -15,6 +15,5 @@ variable "managed_identity_object_id" {
 }
 
 variable "jenkins_AAD_objectId" {
-  type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
