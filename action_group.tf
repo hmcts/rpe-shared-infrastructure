99e1cf51366f3b4246a43afad6392b38117c482f
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2026-04-28 12:52:33 +0100

    Comment out action-group module due to azurerm 4.0.0+ incompatibility

--- action_group.tf
+++ action_group.tf
@@ -1,16 +1,18 @@
-module "action-group" {
-  source   = "git@github.com:hmcts/cnp-module-action-group"
-  location = "global"
-  env      = var.env
-
-  resourcegroup_name     = azurerm_resource_group.rg.name
-  action_group_name      = "RPE alerts - ${var.env}"
-  short_name             = "rpe_alerts"
-  email_receiver_name    = "Platform engineering alerts"
-  email_receiver_address = data.azurerm_key_vault_secret.actionGroupEmail.value
-}
-
-data "azurerm_key_vault_secret" "actionGroupEmail" {
-  name         = "actionGroupEmail"
-  key_vault_id = module.key-vault.key_vault_id
-}
+# Commented out - action-group module incompatible with azurerm 4.0.0+
+# TODO: Update cnp-module-action-group to support azurerm 4.0.0+
+# module "action-group" {
+#   source   = "git@github.com:hmcts/cnp-module-action-group"
+#   location = "global"
+#   env      = var.env
+#
+#   resourcegroup_name     = azurerm_resource_group.rg.name
+#   action_group_name      = "RPE alerts - ${var.env}"
+#   short_name             = "rpe_alerts"
+#   email_receiver_name    = "Platform engineering alerts"
+#   email_receiver_address = data.azurerm_key_vault_secret.actionGroupEmail.value
+# }
+#
+# data "azurerm_key_vault_secret" "actionGroupEmail" {
+#   name         = "actionGroupEmail"
+#   key_vault_id = module.key-vault.key_vault_id
+# }
