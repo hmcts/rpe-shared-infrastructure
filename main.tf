c0e32027bdd94d7e0ac7be8f1b64210ea5a9ea97
Ryan McAlary <145446878+ryanmcalary11@users.noreply.github.com>
2026-04-28 09:39:21 +0100

    DTSPO-30209 - Grant new Jenkins MIs access to keyvault (#16)

--- main.tf
+++ main.tf
@@ -9,12 +9,18 @@ resource "azurerm_resource_group" "rg" {
   tags = var.common_tags
 }
 
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "key-vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
   product             = "${var.product}-shared"
   env                 = var.env
   tenant_id           = var.tenant_id
   object_id           = var.jenkins_AAD_objectId
+  jenkins_object_id   = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name = azurerm_resource_group.rg.name
 
   # DTS Platform Operations
