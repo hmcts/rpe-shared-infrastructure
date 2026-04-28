3764dc3dd4339ec626c2debf8b4f476a32c90e0d
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2026-04-28 12:40:27 +0100

    Update terraform and provider versions to align with infrastructure requirements

--- state.tf
+++ state.tf
@@ -2,9 +2,13 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.52.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.25"
+      version = "4.16.0"
     }
   }
 }
